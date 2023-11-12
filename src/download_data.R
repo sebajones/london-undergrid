a76f6b47ea5b18b57404f1d37643a262753a9898
Seb Jones <gy21saj@leeds.ac.uk>
2023-11-12 09:35:06 +0000

    Create download_data.r

---
+++ src/download_data.R
@@ -0,0 +1,16 @@
+# -------------------------------------------
+# R script for reading and processing Transport for London (TFL) Annual Counts
+# (Entry/Exit) for rail and Tube
+# Author: Seb Jones
+# -------------------------------------------
+
+# -------------------------------------------
+# L I B R A R I E S
+# -------------------------------------------
+
+# Required libraries.
+library(tidyverse)
+
+# -------------------------------------------
+# D O W N L O A D
+# -------------------------------------------
