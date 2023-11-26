d35b0da882901425b80ef31ea4a88fa42f0a9a54
Seb Jones <gy21saj@leeds.ac.uk>
2023-11-26 15:16:28 +0000

    Update script

--- src/download_data.R
+++ src/download_data.R
@@ -1,6 +1,6 @@
 # -------------------------------------------
-# R script for reading and processing Transport for London (TFL) Annual Counts
-# (Entry/Exit) for rail and Tube
+# R script for reading and processing Transport for London (TFL) NUMBAT
+# Entry/Exit statistics for all stations in 2022
 # Author: Seb Jones
 # -------------------------------------------
 
@@ -10,7 +10,17 @@
 
 # Required libraries.
 library(tidyverse)
+library(readxl)
+library(janitor)
 
 # -------------------------------------------
 # D O W N L O A D
 # -------------------------------------------
+
+# Read in NUMBAT 2022
+
+# MON
+
+data_mon <- read_csv("http://crowding.data.tfl.gov.uk/Crowding/LUTrainLoadingData.csv")
+
+
